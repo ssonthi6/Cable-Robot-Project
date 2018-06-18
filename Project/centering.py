8c727865ec9830320439f0617fd3b6d7e3b4e118
Soham Sonthi <thundersplosion@users.noreply.github.com>
2018-06-18 14:20:36 -0400

    Delete centering.py

--- Project/centering.py
+++
@@ -1,9 +0,0 @@
-import cPickle as pickle
-
-def write_origin():
-    pickle.dump([str(21.5), str(52.0)], open('data_pickle.pickle', 'wb'))
-    return "Location centered at zero"
-
-def read_current():
-    test = pickle.load(open('data_pickle.pickle', 'rd'))
-    return test
