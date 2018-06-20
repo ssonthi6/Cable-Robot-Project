2abf37327d46a6353e70c8655407dd3cc74299e7
Soham Sonthi <thundersplosion@users.noreply.github.com>
2018-06-20 10:14:06 -0400

    Added Roboclaw motor

--- Project v2/mqtt_client.py
+++ Project v2/mqtt_client.py
@@ -1,10 +1,13 @@
 import paho.mqtt.client as mqtt
+from roboclaw import Roboclaw
 import cPickle as pickle
 import kinematics as k
 
 MQTT_SERVER = "localhost"
 MQTT_PATH_S = "S_channel"
 MQTT_PATH_T = "T_channel"
+
+rc = Roboclaw("/dev/ttyACM0",115200) #RPI port 1 USB
 
 input_values = []
 
@@ -20,6 +23,11 @@ def on_message(client, userdata, msg):
         print(input_values)
         
         if(len(input_values) == 2):
+                rc.Open()
+                address = 0x80
+
+                rc.SpeedDistanceM1(address, input_values[1], input_values[0], 0)
+              
                 input_values[:] = []
                 
         
