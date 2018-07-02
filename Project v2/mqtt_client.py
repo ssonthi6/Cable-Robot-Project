5a330e35cf16bcccb728036cfdc1260565948d89
Soham Sonthi <thundersplosion@users.noreply.github.com>
2018-07-02 12:51:23 -0400

    Add files via upload

--- Project v2/mqtt_client.py
+++ Project v2/mqtt_client.py
@@ -1,15 +1,19 @@
 import paho.mqtt.client as mqtt
+import math
 from roboclaw import Roboclaw
 import cPickle as pickle
-import kinematics as k
 
 MQTT_SERVER = "localhost"
 MQTT_PATH_S = "S_channel"
 MQTT_PATH_T = "T_channel"
 
 rc = Roboclaw("/dev/ttyACM0",115200) #RPI port 1 USB
+rc.Open()
+address = 0x80
 
 input_values = []
+
+enc_values = []
 
 def on_connect(client, userdata, flags, rc):
         print("connected with result code " + str(rc))
@@ -23,11 +27,22 @@ def on_message(client, userdata, msg):
         print(input_values)
         
         if(len(input_values) == 2):
-                rc.Open()
-                address = 0x80
+                bool_init = True
+                while(bool_init):
+                        rc.SpeedM1(address, int(input_values[1]))
+                        enc = rc.ReadEncM1(address)
+                        enc_values.append(enc[1])
+                        total_distance_gone = math.fabs(enc_values[-1]
+                                                        - enc_values[0])
+                        correction_dist = int(int(input_values[0])
+                                              - total_distance_gone)
 
-                rc.SpeedDistanceM1(address, input_values[1], input_values[0], 0)
-              
+                        if(total_distance_gone >= int(input_values[0])):
+                                rc.SpeedM1(address, 0)
+                                enc_values[:] = []
+                                bool_init = False
+
+                print("tics off: " + str(correction_dist))
                 input_values[:] = []
                 
         
